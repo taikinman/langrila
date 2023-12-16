c61cc6384d231028efb0e7ee8629db6969e80b7f
Rio Fujita <rio_github@rio.st>
2023-12-16 09:37:56 +0900

    Update cosmos.py
    Changed the name of arguments to clarify their role to point environment variables
    Changed uuid to hashlib
    Changed return values to align to JSONConversationMemory class

--- src/langrila/memory/cosmos.py
+++ src/langrila/memory/cosmos.py
@@ -1,17 +1,16 @@
 from azure.cosmos import exceptions, CosmosClient, PartitionKey
 import logging
 import os
-import uuid
+import hashlib
 
 from ..base import BaseConversationMemory
 
 class CosmosConversationMemory(BaseConversationMemory):
-    def __init__(self, endpoint_name: str, key_name: str, dbname_name: str, containername_name: str, pkey_name: str):
-        self.endpoint = os.getenv(endpoint_name)
-        self.key = os.getenv(key_name)
-        self.dbname = os.getenv(dbname_name)
-        self.containername = os.getenv(containername_name)
-        self.pkey = '/' + os.getenv(pkey_name).strip('/')
+    def __init__(self, endpoint_env_name: str, key_env_name: str, db_env_name: str, container_env_name: str):
+        self.endpoint = os.getenv(endpoint_env_name)
+        self.key = os.getenv(key_env_name)
+        self.dbname = os.getenv(db_env_name)
+        self.containername = os.getenv(container_env_name)
         # Create a Cosmos client
         try:
             client = CosmosClient(url=self.endpoint, credential=self.key)
@@ -31,15 +30,18 @@ class CosmosConversationMemory(BaseConversationMemory):
 
     def store(self, conversation_history: list[dict[str, str]]):
         for item in conversation_history:
-            item['id'] = str(uuid.uuid4())
-            self.container.create_item(item)
-
+            item["id"] = hashlib.sha256(str(item).encode()).hexdigest()
+            try:
+                self.container.create_item(item)
+            except exceptions.CosmosResourceExistsError:
+                pass
+    
     def load(self) -> list[dict[str, str]]:
         result = []
         try:
-            items = self.container.read_items()
+            items = self.container.read_all_items()
             for item in items:
-                result.append(item)
+                result.append({k: v for k, v in item.items() if k in {"role", "content"}})
             return result
         except:
             return result
