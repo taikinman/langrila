338bbf32204d6989cae8bfb705ba12acd913ee2a
Rio Fujita <rio_github@rio.st>
2023-12-16 09:29:21 +0900

    Update __init__.py
    Removed import to avoid adding dependencies.

--- src/langrila/__init__.py
+++ src/langrila/__init__.py
@@ -9,7 +9,6 @@ from .chat_module.function_calling import (
 )
 from .embedding.openai import OpenAIEmbeddingModule
 from .memory.json import JSONConversationMemory
-from .memory.cosmos import CosmosConversationMemory
 from .message import Message
 from .prompt_template import PromptTemplate
 from .usage import Usage
