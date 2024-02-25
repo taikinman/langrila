ee784a060c4bd96f36c6f837bebe70756e6233d6
taikinman <okipedia6161@gmail.com>
2024-02-25 09:55:00 +0900

    add in-memory conversation memory

---
+++ src/langrila/memory/in_memory.py
@@ -0,0 +1,14 @@
+import copy
+
+from ..base import BaseConversationMemory
+
+
+class InMemoryConversationMemory(BaseConversationMemory):
+    def __init__(self):
+        self.history = []
+
+    def store(self, conversation_history: list[dict[str, str]]):
+        self.history = copy.deepcopy(conversation_history)
+
+    def load(self) -> list[dict[str, str]]:
+        return self.history
