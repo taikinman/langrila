53b96fc8f3f004c5c0d0ad3879d2cdc8fa4219a6
taikinman <okipedia6161@gmail.com>
2024-03-11 09:31:52 +0900

    refactor

--- src/langrila/logger.py
+++ src/langrila/logger.py
@@ -3,26 +3,72 @@ import logging
 import os
 
 
-class DefaultLogger:
-    def __init__(self, path: str, name: str = "log", level: str = "INFO"):
+class Logger:
+    def __init__(self, path: str, name: str = "log", level: str = "DEBUG", include_timestamp: bool = False):
+        path_log = os.path.join(path, f"{name}.log")
+        if os.path.exists(path_log):
+            os.remove(path_log)
+
         if not os.path.exists(path):
             os.makedirs(path, exist_ok=True)
 
-        self.general_logger = logging.getLogger(path)
+        self.general_logger = logging.getLogger(path_log)
         stream_handler = logging.StreamHandler()
-        file_general_handler = logging.FileHandler(os.path.join(path, f"{name}.txt"))
-        if len(self.general_logger.handlers) == 0:
-            self.general_logger.addHandler(stream_handler)
-            self.general_logger.addHandler(file_general_handler)
-            self.general_logger.setLevel(getattr(logging, level))
+        file_general_handler = logging.FileHandler(path_log)
 
-    def info(self, message):
-        # display time
-        self.general_logger.info("[{}] - {}".format(self.now_string(), message))
+        if len(self.general_logger.handlers) > 0:
+            self.general_logger.handlers.clear()
 
-    def debug(self, message):
-        # display time
-        self.general_logger.debug("[{}] - {}".format(self.now_string(), message))
+        self.general_logger.addHandler(stream_handler)
+        self.general_logger.addHandler(file_general_handler)
+        self.general_logger.setLevel(getattr(logging, level))
+
+        self.include_timestamp = include_timestamp
+
+    def critical(self, message: str):
+        if self.include_timestamp:
+            message = self._add_timestamp(message)
+            self.general_logger.critical(message)
+        else:
+            self.general_logger.critical(message)
+
+    def fatal(self, message: str):
+        if self.include_timestamp:
+            message = self._add_timestamp(message)
+            self.general_logger.fatal(message)
+        else:
+            self.general_logger.fatal(message)
+
+    def error(self, message: str):
+        if self.include_timestamp:
+            message = self._add_timestamp(message)
+            self.general_logger.error(message)
+        else:
+            self.general_logger.error(message)
+
+    def warning(self, message: str):
+        if self.include_timestamp:
+            message = self._add_timestamp(message)
+            self.general_logger.warning(message)
+        else:
+            self.general_logger.warning(message)
+
+    def info(self, message: str):
+        if self.include_timestamp:
+            message = self._add_timestamp(message)
+            self.general_logger.info(message)
+        else:
+            self.general_logger.info(message)
+
+    def debug(self, message: str):
+        if self.include_timestamp:
+            message = self._add_timestamp(message)
+            self.general_logger.debug(message)
+        else:
+            self.general_logger.debug(message)
+
+    def _add_timestamp(self, message: str):
+        return f"[{self.now_string()}] {message}"
 
     @staticmethod
     def now_string():
