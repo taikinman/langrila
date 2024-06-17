568714c5366d4aad41994023b452e0caf7cdc434
Nsak <101960796+taikinman@users.noreply.github.com>
2024-06-17 09:14:21 +0900

    Support chroma http client and tweak arguments (#36)

--- src/langrila/database/chroma.py
+++ src/langrila/database/chroma.py
@@ -1,7 +1,7 @@
 from typing import Any
 
 import chromadb
-from chromadb import DEFAULT_DATABASE, DEFAULT_TENANT
+from chromadb import DEFAULT_DATABASE, DEFAULT_TENANT, Settings
 from chromadb.api import ClientAPI
 from chromadb.types import Where
 
@@ -10,6 +10,8 @@ from .base import (
     BaseEmbeddingModule,
     BaseLocalCollectionModule,
     BaseLocalRetrievalModule,
+    BaseRemoteCollectionModule,
+    BaseRemoteRetrievalModule,
 )
 
 
@@ -21,6 +23,7 @@ class ChromaLocalCollectionModule(BaseLocalCollectionModule):
         metadata: dict[str, Any] | None = None,
         embedder: BaseEmbeddingModule | None = None,
         logger: Any | None = None,
+        settings: Settings | None = None,
         tenant: str = DEFAULT_TENANT,
         database: str = DEFAULT_DATABASE,
     ):
@@ -31,6 +34,7 @@ class ChromaLocalCollectionModule(BaseLocalCollectionModule):
             logger=logger,
         )
         self.metadata = metadata or {"hnsw:space": "cosine"}
+        self.settings = settings
         self.tenant = tenant
         self.database = database
 
@@ -80,7 +84,10 @@ class ChromaLocalCollectionModule(BaseLocalCollectionModule):
 
     def get_client(self) -> ClientAPI:
         return chromadb.PersistentClient(
-            path=self.persistence_directory.as_posix(), tenant=self.tenant, database=self.database
+            path=self.persistence_directory.as_posix(),
+            settings=self.settings,
+            tenant=self.tenant,
+            database=self.database,
         )
 
     def as_retriever(
@@ -92,9 +99,161 @@ class ChromaLocalCollectionModule(BaseLocalCollectionModule):
             embedder=self.embedder,
             persistence_directory=self.persistence_directory,
             collection_name=self.collection_name,
+            settings=self.settings,
+            tenant=self.tenant,
+            database=self.database,
             n_results=n_results,
             score_threshold=score_threshold,
             logger=self.logger,
+        )
+
+
+class ChromaRemoteCollectionModule(BaseRemoteCollectionModule):
+    def __init__(
+        self,
+        host: str,
+        collection_name: str,
+        port: str = "8000",
+        ssl: bool = False,
+        headers: dict[str, str] | None = None,
+        settings: Settings | None = None,
+        metadata: dict[str, Any] | None = None,
+        embedder: BaseEmbeddingModule | None = None,
+        logger: Any | None = None,
+        tenant: str = DEFAULT_TENANT,
+        database: str = DEFAULT_DATABASE,
+    ):
+        super().__init__(
+            url=host,
+            port=port,
+            collection_name=collection_name,
+            embedder=embedder,
+            logger=logger,
+        )
+        self.metadata = metadata or {"hnsw:space": "cosine"}
+        self.tenant = tenant
+        self.database = database
+        self.ssl = ssl
+        self.headers = headers
+        self.settings = settings
+
+    def _exists(self, client: ClientAPI, collection_name: str) -> bool:
+        return len([c.name for c in client.list_collections() if c.name == collection_name]) > 0
+
+    def _create_collection(self, client: ClientAPI, collection_name: str) -> None:
+        self.collection = client.create_collection(name=collection_name, metadata=self.metadata)
+
+    def _delete_record(
+        self,
+        client: ClientAPI,
+        collection_name: str,
+        ids: list[str | int],
+        filter: Where | None = None,
+        **kwargs,
+    ) -> None:
+        if not hasattr(self, "collection"):
+            self.collection = client.get_collection(name=collection_name)
+        self.collection.delete(ids=[str(i) for i in ids], where=filter, **kwargs)
+
+    def _upsert(
+        self,
+        client: ClientAPI,
+        collection_name: str,
+        ids: list[str | int],
+        documents: list[str],
+        embeddings: list[list[float]],
+        metadatas: list[dict[str, str]],
+        **kwargs,
+    ) -> None:
+        if not hasattr(self, "collection"):
+            self.collection = client.get_collection(name=collection_name)
+
+        self.collection.upsert(
+            ids=[str(i) for i in ids],
+            embeddings=embeddings,
+            documents=documents,
+            metadatas=[m | {"document": doc} for m, doc in zip(metadatas, documents, strict=True)],
+            **kwargs,
+        )
+
+        return
+
+    def _delete_collection(self, client: ClientAPI, collection_name: str) -> None:
+        client.delete_collection(name=collection_name)
+
+    def get_client(self) -> ClientAPI:
+        return chromadb.HttpClient(
+            host=self.url,
+            port=self.port,
+            ssl=self.ssl,
+            headers=self.headers,
+            settings=self.settings,
+            tenant=self.tenant,
+            database=self.database,
+        )
+
+    def get_async_client(self) -> ClientAPI:
+        return self.get_client()
+
+    def as_retriever(
+        self,
+        n_results: int = 4,
+        score_threshold: float = 0.5,
+    ) -> "ChromaRemoteRetrievalModule":
+        return ChromaRemoteRetrievalModule(
+            embedder=self.embedder,
+            host=self.url,
+            port=self.port,
+            ssl=self.ssl,
+            headers=self.headers,
+            settings=self.settings,
+            collection_name=self.collection_name,
+            n_results=n_results,
+            score_threshold=score_threshold,
+            logger=self.logger,
+            tenant=self.tenant,
+            database=self.database,
+        )
+
+    async def _acreate_collection(self, client: ClientAPI, collection_name: str) -> None:
+        self._create_collection(client=client, collection_name=collection_name)
+
+    async def _aexists(self, client: Any, collection_name: str) -> bool:
+        return self._exists(client=client, collection_name=collection_name)
+
+    async def _adelete_collection(self, client: ClientAPI, collection_name: str) -> None:
+        self._delete_collection(client=client, collection_name=collection_name)
+
+    async def _adelete_record(
+        self,
+        client: ClientAPI,
+        collection_name: str,
+        ids: list[str | int],
+        filter: Where | None = None,
+        **kwargs,
+    ) -> None:
+        self._delete_record(
+            client=client, collection_name=collection_name, ids=ids, filter=filter, **kwargs
+        )
+
+    async def _aupsert(
+        self,
+        client: ClientAPI,
+        collection_name: str,
+        ids: list[str | int],
+        documents: list[str],
+        embeddings: list[list[float]],
+        metadatas: list[dict[str, str]],
+        **kwargs,
+    ) -> None:
+        self._upsert(
+            client=client,
+            collection_name=collection_name,
+            ids=ids,
+            documents=documents,
+            embeddings=embeddings,
+            metadatas=metadatas,
+            **kwargs,
         )
 
 
@@ -108,6 +267,7 @@ class ChromaLocalRetrievalModule(BaseLocalRetrievalModule):
         score_threshold: float = 0.5,
         logger: Any | None = None,
         ascending: bool = True,
+        settings: Settings | None = None,
         tenant: str = DEFAULT_TENANT,
         database: str = DEFAULT_DATABASE,
     ):
@@ -120,12 +280,16 @@ class ChromaLocalRetrievalModule(BaseLocalRetrievalModule):
             logger=logger,
             ascending=ascending,
         )
+        self.settings = settings
         self.tenant = tenant
         self.database = database
 
     def get_client(self) -> ClientAPI:
         return chromadb.PersistentClient(
-            path=self.persistence_directory.as_posix(), tenant=self.tenant, database=self.database
+            path=self.persistence_directory.as_posix(),
+            settings=self.settings,
+            tenant=self.tenant,
+            database=self.database,
         )
 
     def _retrieve(
@@ -175,4 +339,121 @@ class ChromaLocalRetrievalModule(BaseLocalRetrievalModule):
             documents=documents,
             metadatas=metadatas,
             collections=collections,
+        )
+
+
+class ChromaRemoteRetrievalModule(BaseRemoteRetrievalModule):
+    def __init__(
+        self,
+        host: str,
+        collection_name: str,
+        port: str = "8000",
+        ssl: bool = False,
+        headers: dict[str, str] | None = None,
+        settings: Settings | None = None,
+        embedder: BaseEmbeddingModule | None = None,
+        n_results: int = 4,
+        score_threshold: float = 0.5,
+        logger: Any | None = None,
+        ascending: bool = True,
+        tenant: str = DEFAULT_TENANT,
+        database: str = DEFAULT_DATABASE,
+    ):
+        super().__init__(
+            url=host,
+            port=port,
+            collection_name=collection_name,
+            embedder=embedder,
+            n_results=n_results,
+            score_threshold=score_threshold,
+            logger=logger,
+            ascending=ascending,
+        )
+        self.ssl = ssl
+        self.headers = headers
+        self.settings = settings
+        self.tenant = tenant
+        self.database = database
+
+    def get_client(self) -> ClientAPI:
+        return chromadb.HttpClient(
+            host=self.url,
+            port=self.port,
+            ssl=self.ssl,
+            headers=self.headers,
+            settings=self.settings,
+            tenant=self.tenant,
+            database=self.database,
+        )
+
+    def get_async_client(self) -> ClientAPI:
+        return self.get_client()
+
+    def _retrieve(
+        self,
+        client: ClientAPI,
+        collection_name: str,
+        query_vector: list[float],
+        n_results: int,
+        score_threshold: float,
+        filter: Any | None = None,
+        **kwargs,
+    ) -> RetrievalResults:
+        if not hasattr(self, "collection"):
+            self.collection = client.get_collection(name=collection_name)
+
+        retrieved = self.collection.query(
+            query_embeddings=query_vector,
+            where=filter,
+            include=["metadatas", "documents", "distances"],
+            n_results=n_results,
+            **kwargs,
+        )
+
+        ids = []
+        scores = []
+        documents = []
+        metadatas = []
+        collections = []
+
+        for _id, dist, document, metadata in zip(
+            retrieved["ids"][0],
+            retrieved["distances"][0],
+            retrieved["documents"][0],
+            retrieved["metadatas"][0],
+            strict=True,
+        ):
+            if dist <= score_threshold:
+                ids.append(int(_id))
+                scores.append(dist)
+                documents.append(document)
+                metadatas.append(metadata)
+                collections.append(collection_name)
+
+        return RetrievalResults(
+            ids=ids,
+            scores=scores,
+            documents=documents,
+            metadatas=metadatas,
+            collections=collections,
+        )
+
+    async def _aretrieve(
+        self,
+        client: ClientAPI,
+        collection_name: str,
+        query_vector: list[float],
+        n_results: int,
+        score_threshold: float,
+        filter: Any | None = None,
+        **kwargs,
+    ) -> RetrievalResults:
+        return self._retrieve(
+            client=client,
+            collection_name=collection_name,
+            query_vector=query_vector,
+            n_results=n_results,
+            score_threshold=score_threshold,
+            filter=filter,
+            **kwargs,
         )
