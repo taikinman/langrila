c3a8467b335093e48beb859ad0b989ff4f7ea82c
taikinman <okipedia6161@gmail.com>
2024-07-22 09:28:20 +0900

    enhance base message class to handle universal message class

--- src/langrila/base.py
+++ src/langrila/base.py
@@ -1,7 +1,23 @@
 from abc import ABC, abstractmethod
-from typing import Any, AsyncGenerator, Generator
+from pathlib import Path
+from typing import Any, AsyncGenerator, Generator, Literal
 
+from PIL import Image
+
+from langrila.result import ToolCallResponse, ToolOutput
+
+from .message_content import (
+    ApplicationFileContent,
+    ContentType,
+    ImageContent,
+    Message,
+    TextContent,
+    ToolCall,
+    ToolContent,
+)
 from .result import CompletionResults, EmbeddingResults, FunctionCallingResults
+from .types import PathType
+from .utils import decode_image
 
 
 class BaseChatModule(ABC):
@@ -69,15 +85,11 @@ class BaseConversationMemory(ABC):
 
 
 class BaseMessage(ABC):
-    def __init__(
-        self, content: str, images: Any | list[Any] | None = None, name: str | None = None
-    ):
-        self.content = content
-        self.images = images
+    def __init__(self, contents: list[dict[str, Any]], name: str | None = None):
+        self.contents = contents
         self.name = name
 
     @property
-    @abstractmethod
     def as_system(self):
         raise NotImplementedError
 
@@ -92,13 +104,253 @@ class BaseMessage(ABC):
         raise NotImplementedError
 
     @property
-    @abstractmethod
     def as_function(self):
         raise NotImplementedError
 
-    # @property
-    # def as_tool(self):
-    #     return {"role": "tool", "content": self.content}
+    @staticmethod
+    def _get_image_from_content(content: ImageContent) -> Image.Image:
+        if isinstance(content.image, Image.Image):
+            return content.image
+        elif isinstance(content.image, bytes):
+            return decode_image(content.image, as_utf8=False)
+        elif isinstance(content.image, PathType):
+            try:
+                assert Path(content.image).is_file(), f"{content.image} is not a file"
+                return Image.open(content.image)
+            except OSError:
+                return decode_image(content.image, as_utf8=True)
+
+    @staticmethod
+    @abstractmethod
+    def _format_text_content(content: TextContent) -> Any:
+        raise NotImplementedError
+
+    @staticmethod
+    def _format_image_content(content: ImageContent) -> Any:
+        raise NotImplementedError
+
+    @staticmethod
+    def _format_tool_content(content: ToolContent) -> Any:
+        raise NotImplementedError
+
+    @staticmethod
+    def _format_tool_call_content(content: ToolCall) -> Any:
+        raise NotImplementedError
+
+    @staticmethod
+    def _format_application_file_content(content: ApplicationFileContent) -> Any:
+        raise NotImplementedError
+
+    @classmethod
+    @abstractmethod
+    def _from_completion_results(cls, results: CompletionResults) -> list[dict[str, str]]:
+        raise NotImplementedError
+
+    @classmethod
+    def _from_completion_results(cls, response: CompletionResults) -> Message:
+        return cls.from_client_message(response.message)
+
+    @classmethod
+    def _from_function_calling_results(
+        cls, response: FunctionCallingResults
+    ) -> list[dict[str, Any]]:
+        return [
+            Message(
+                role="function",  # global role
+                content=[
+                    ToolContent(
+                        output=result.output,
+                        args=result.args,
+                        call_id=result.call_id,
+                        funcname=result.funcname,
+                    )
+                ],
+                name=result.funcname,
+            )
+            if isinstance(result, ToolOutput)
+            else Message(
+                role="assistant",  # global role
+                content=[result],
+            )
+            for result in response.results
+        ]
+
+    @classmethod
+    def _from_function_calling_calls(cls, response: FunctionCallingResults) -> list[dict[str, Any]]:
+        return Message(
+            role="function_call",  # global role
+            content=[
+                ToolCall(
+                    args=result.args,
+                    name=result.name,
+                    call_id=result.call_id,
+                )
+                if isinstance(result, ToolCallResponse)
+                else result
+                for result in response.calls
+            ],
+        )
+
+    @classmethod
+    def _format_string_content(cls, content: str) -> Any:
+        if Path(content).is_file() and Path(content).suffix in [".png", ".jpg", ".jpeg"]:
+            return cls._format_image_content(content=ImageContent(image=content))
+        elif Path(content).is_file() and Path(content).suffix in [".pdf"]:
+            return cls._format_application_file_content(
+                content=ApplicationFileContent(file=content)
+            )
+        else:
+            return cls._format_text_content(content=TextContent(text=content))
+
+    @classmethod
+    def _format_contents(
+        cls,
+        contents: ContentType | list[ContentType],
+    ) -> list[dict[str, Any]]:
+        if not isinstance(contents, list):
+            contents = [contents]
+
+        _contents = []
+
+        for content in contents:
+            if isinstance(content, str):
+                _contents.append(cls._format_string_content(content=content))
+            elif isinstance(content, TextContent):
+                _contents.append(cls._format_text_content(content=content))
+            elif isinstance(content, ImageContent):
+                try:
+                    _contents.append(cls._format_image_content(content=content))
+                except NotImplementedError:
+                    pass
+            elif isinstance(content, ToolContent):
+                try:
+                    _contents.append(cls._format_tool_content(content=content))
+                except NotImplementedError:
+                    pass
+            elif isinstance(content, ToolCall):
+                try:
+                    _contents.append(cls._format_tool_call_content(content=content))
+                except NotImplementedError:
+                    pass
+            elif isinstance(content, ApplicationFileContent):
+                try:
+                    _contents.append(cls._format_application_file_content(content=content))
+                except NotImplementedError:
+                    pass
+            else:
+                raise NotImplementedError(f"Message type {type(content)} not implemented")
+
+        return _contents
+
+    @classmethod
+    def _format_message(
+        cls,
+        role: Literal["user", "assistant", "system", "function", "function_call"],
+        contents: ContentType | list[ContentType],
+        name: str | None = None,
+    ) -> Any:
+        if role not in ["user", "assistant", "system", "function", "function_call"]:
+            raise ValueError(f"Invalid role: {role}")
+
+        _contents = cls._format_contents(contents=contents)
+
+        return getattr(cls(contents=_contents, name=name), "as_" + role)
+
+    @classmethod
+    def to_client_message(
+        cls,
+        message: Message,
+    ) -> Any:
+        return cls._format_message(role=message.role, contents=message.content, name=message.name)
+
+    @classmethod
+    @abstractmethod
+    def from_client_message(cls, message: Any) -> Message:
+        raise NotImplementedError
+
+    @classmethod
+    def _string2content(cls, content: str) -> ContentType:
+        if Path(content).is_file() and Path(content).suffix in [".png", ".jpg", ".jpeg"]:
+            return ImageContent(image=content)
+        elif Path(content).is_file() and Path(content).suffix in [".pdf"]:
+            return ApplicationFileContent(file=content)
+        else:
+            return TextContent(text=content)
+
+    @classmethod
+    def to_contents(
+        cls,
+        contents: ContentType | list[ContentType],
+    ) -> list[ContentType]:
+        if not isinstance(contents, list):
+            contents = [contents]
+
+        _contents = []
+
+        for content in contents:
+            if isinstance(content, str):
+                _contents.append(cls._string2content(content=content))
+            elif isinstance(
+                content, (TextContent, ImageContent, ToolContent, ApplicationFileContent, ToolCall)
+            ):
+                _contents.append(content)
+            else:
+                raise NotImplementedError(f"Message type {type(content)} not implemented")
+
+        return _contents
+
+    @classmethod
+    def to_universal_message(
+        cls,
+        message: Message | ContentType | dict[str, Any],
+        role: Literal["system", "user", "assistant", "function", "function_call"] | None = None,
+        name: str | None = None,
+    ) -> Message:
+        if isinstance(message, Message):
+            message.content = cls.to_contents(contents=message.content)
+            return message
+        elif isinstance(message, ContentType):
+            if role is None:
+                raise ValueError("Role must be provided to create a message")
+
+            message = Message(role=role, content=message, name=name)
+            message.content = cls.to_contents(contents=message.content)
+            return message
+        elif isinstance(message, list):
+            if role is None:
+                raise ValueError("Role must be provided to create a message")
+
+            for content in message:
+                if not isinstance(content, ContentType):
+                    raise ValueError(f"Invalid content type {type(content)} in the input list.")
+
+            message = Message(role=role, content=message, name=name)
+            message.content = cls.to_contents(contents=message.content)
+            return message
+        elif isinstance(message, dict):
+            message = Message(**message)
+            message.content = cls.to_contents(contents=message.content)
+            return message
+        else:
+            raise ValueError(f"Invalid message type {type(message)}")
+
+    @classmethod
+    def to_universal_message_from_completion_response(
+        cls, response: CompletionResults
+    ) -> dict[str, Any]:
+        return cls._from_completion_results(response)
+
+    @classmethod
+    def to_universal_message_from_function_response(
+        cls, response: FunctionCallingResults
+    ) -> dict[str, Any]:
+        return cls._from_function_calling_results(response)
+
+    @classmethod
+    def to_universal_message_from_function_call(
+        cls, response: FunctionCallingResults
+    ) -> dict[str, Any]:
+        return cls._from_function_calling_calls(response)
 
 
 class BaseMetadataStore(ABC):
