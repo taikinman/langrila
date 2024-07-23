64d99d15a30f1cefe7684e2cfbb2fc086ff06f8b
taikinman <okipedia6161@gmail.com>
2024-07-23 11:52:14 +0900

    fix import

--- src/langrila/openai/__init__.py
+++ src/langrila/openai/__init__.py
@@ -2,10 +2,8 @@ from .embedding import OpenAIEmbeddingModule
 from .llm.chat import OpenAIChatModule
 from .llm.function_calling import (
     OpenAIFunctionCallingModule,
-    ToolConfig,
     ToolOutput,
-    ToolParameter,
-    ToolProperty,
 )
 from .message import OpenAIMessage
 from .model import ChatGPT
+from .tools import ToolConfig, ToolParameter, ToolProperty
