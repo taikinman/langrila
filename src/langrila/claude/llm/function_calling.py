34cf868c94305da22cf7bb39b264c7c503a7a1b6
Nsak <101960796+taikinman@users.noreply.github.com>
2024-07-28 23:55:28 +0900

    Fix type hint of _get_client_message_type() (#84)

--- src/langrila/claude/llm/function_calling.py
+++ src/langrila/claude/llm/function_calling.py
@@ -364,5 +364,5 @@ class AnthropicFunctionCallingModule(FunctionCallingWrapperModule):
             token_counter=token_counter,
         )
 
-    def _get_client_message_type(self) -> type[BaseMessage]:
+    def _get_client_message_type(self) -> ClaudeMessage:
         return ClaudeMessage
