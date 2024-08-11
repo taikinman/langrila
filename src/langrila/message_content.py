2e4364bce3f873ab6cd37f56b451e2138028aee4
Nsak <101960796+taikinman@users.noreply.github.com>
2024-08-11 22:59:46 +0900

    Support capability to understand PDF, Video and URI contents (#108)

--- src/langrila/message_content.py
+++ src/langrila/message_content.py
@@ -4,7 +4,7 @@ from typing import Any, Literal
 from PIL import Image
 from pydantic import BaseModel, ConfigDict, model_validator
 
-from .types import FileType, PathType
+from .types import PathType
 from .utils import encode_image
 
 
@@ -22,7 +22,7 @@ class ImageContent(BaseModel):
         if isinstance(self.image, Image.Image):
             return self.image.format.lower()
         elif isinstance(self.image, PathType):
-            return Path(self.image).suffix.lstrip(".")
+            return Path(self.image).suffix.lstrip(".").lower()
         else:
             raise ValueError("Invalid image type")
 
@@ -37,7 +37,7 @@ class ImageContent(BaseModel):
                 try:
                     assert Path(data["image"]).is_file()
                     data["image"] = encode_image(Image.open(data["image"]), as_utf8=True)
-                except OSError:
+                except Exception:
                     pass
 
         return data
@@ -58,12 +58,69 @@ class ToolContent(BaseModel):
     funcname: str | None = None
 
 
-class ApplicationFileContent(BaseModel):
+class PDFContent(BaseModel):
     model_config = ConfigDict(arbitrary_types_allowed=True)
-    file: FileType
+    file: PathType
+    resolution_scale: float = 2.5
+    image_resolution: Literal["auto", "low", "high"] = "high"
+
+    def as_image_content(self) -> list[ImageContent]:
+        from .file_utils.pdf import read_pdf_asimage
+
+        return [
+            ImageContent(image=image, resolution=self.image_resolution)
+            for image in read_pdf_asimage(self.file, scale=self.resolution_scale)
+        ]
 
 
-ContentType = str | TextContent | ApplicationFileContent | ToolContent | ImageContent | ToolCall
+class VideoContent(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+    file: PathType
+    fps: float = 1.0
+    image_resolution: Literal["auto", "low", "high"] = "low"
+
+    def as_image_content(self) -> list[ImageContent]:
+        from .file_utils.video import sample_frames
+
+        return [
+            ImageContent(image=image, resolution=self.image_resolution)
+            for image in sample_frames(self.file, fps=self.fps)
+        ]
+
+
+class URIContent(BaseModel):
+    uri: str
+    mime_type: str | None = None
+
+    @model_validator(mode="before")
+    def check_mime_type(cls, data):
+        if isinstance(data, dict) and "uri" in data:
+            file_format = Path(data["uri"]).suffix.lstrip(".").lower()
+            if file_format in ["jpg", "jpeg", "png", "heic", "heif"]:
+                if file_format == "jpg":
+                    file_format = "jpeg"
+                data["mime_type"] = f"image/{file_format}"
+            elif file_format in ["pdf", "json"]:
+                data["mime_type"] = f"application/{file_format}"
+            elif file_format in ["wav", "mp3", "aiff", "aac", "ogg", "flac"]:
+                data["mime_type"] = f"audio/{file_format}"
+            elif file_format in ["mp4", "mpeg", "mov", "avi", "wmv", "mpg"]:
+                data["mime_type"] = f"video/{file_format}"
+            elif file_format in ["txt", "html", "csv", "markdown", "xml"]:
+                data["mime_type"] = f"text/{file_format}"
+        return data
+
+
+ContentType = (
+    str
+    | TextContent
+    | PDFContent
+    | ToolContent
+    | ImageContent
+    | ToolCall
+    | URIContent
+    | VideoContent
+)
 
 
 class Message(BaseModel):
