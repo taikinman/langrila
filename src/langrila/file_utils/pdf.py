ce7a0786c54c241f1a3f2b65d99544c8f92476a6
taikinman <okipedia6161@gmail.com>
2024-08-11 22:19:16 +0900

    Add file_utils

---
+++ src/langrila/file_utils/pdf.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+from typing import Generator
+
+import pypdfium2
+from PIL import Image
+
+
+def read_pdf_asimage(
+    path: Path | str | bytes, scale: float = 2.5
+) -> Generator[Image.Image, None, None]:
+    pdf = pypdfium2.PdfDocument(path)
+
+    for page in pdf:
+        pil_image = page.render(
+            scale=scale,
+            rotation=0,
+            no_smoothtext=False,
+            no_smoothpath=False,
+            no_smoothimage=False,
+            prefer_bgrx=False,
+        ).to_pil()
+
+        yield pil_image
