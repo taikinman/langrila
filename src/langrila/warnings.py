3badcc65885414f2a03d92f25da67f0e7087137f
taikinman <okipedia6161@gmail.com>
2024-10-21 09:06:04 +0900

    update deprecated warnings decorator

--- src/langrila/warnings.py
+++ src/langrila/warnings.py
@@ -1,17 +1,24 @@
 import functools
-import logging
 import warnings
+
+warnings.simplefilter("default", DeprecationWarning)
 
 
 def custom_formatwarning(message, category, filename, lineno, line=None):
-    return f"{category.__name__}: {message}\n"
+    return f"[{category.__name__}]:{message}\n"
 
 
-LOGGER = logging.getLogger(__name__)
+warnings.formatwarning = custom_formatwarning
 
 
 def deprecated_argument(
-    arg: str, move_to: str | None = None, removal: str | None = None, since: str | None = None
+    arg: str,
+    move_to: str | None = None,
+    removal: str | None = None,
+    since: str | None = None,
+    alternative: str | None = None,
+    module_name: str | None = None,
+    details: str | None = None,
 ):
     """
     Decorator to mark arguments as obsolete.
@@ -26,23 +33,34 @@ def deprecated_argument(
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
             if arg in kwargs and kwargs[arg]:
+                message = ""
                 if move_to:
-                    message = f"Argument '{arg}' will be moved to {move_to}. Please specify this argument in {move_to}."
+                    message += f"Argument '{arg}' will be moved to {move_to}. Please specify this argument in {move_to}."
                 else:
-                    message = (
-                        f"Argument '{arg}' is deprecated and will be removed in future versions."
-                    )
+                    if since:
+                        message += f" {arg} in {func.__name__}() will be deprecated since {since}"
 
-                if since:
-                    message += f" {arg} in {func.__name__} will be deprecated since {since}"
+                        if removal:
+                            message += f" and will be removed in version {removal}"
+                    else:
+                        message = (
+                            f"Argument '{arg}' is deprecated and will be removed in future versions"
+                        )
 
-                    if removal:
-                        message += f" and will be removed in version {removal}."
+                    if module_name:
+                        message += f" for {module_name}"
 
                 if not message.endswith("."):
                     message += "."
 
-                LOGGER.warning(message)
+                if alternative:
+                    message += f" Use '{alternative}' instead."
+
+                if details:
+                    message += f" For more details, see: {details}."
+
+                warnings.warn(message, DeprecationWarning, stacklevel=2)
+
             return func(*args, **kwargs)
 
         return wrapper
