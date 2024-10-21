4b12ea92dfee405227f527e9664414cb2d16d560
taikinman <okipedia6161@gmail.com>
2024-10-21 15:38:39 +0900

    Impl GeminiEmbeddingModule

--- src/langrila/gemini/__init__.py
+++ src/langrila/gemini/__init__.py
@@ -1,4 +1,6 @@
 from .assembly import GeminiFunctionalChat
+
+# from .message.gemini_api import GeminiMessage
+from .embedding import GeminiEmbeddingModule
 from .llm.chat import GeminiChatModule
 from .llm.function_calling import GeminiFunctionCallingModule
-# from .message.gemini_api import GeminiMessage
